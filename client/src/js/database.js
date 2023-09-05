d2831ced7da3f817068649264bcfef3f8d4ff777
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-09-04 22:48:53 -0500

    added put call to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Function to put content into the database
+export const putDb = async (content) => {
+  console.log('Putting content into the database');
+
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  try {
+    // Add the content to the object store.
+    const result = await store.add({ content });
+
+    // Check if the content was successfully added
+    if (result) {
+      console.log('Content added successfully:', result);
+    } else {
+      console.error('Failed to add content to the database');
+    }
+  } catch (error) {
+    console.error('Error adding content to the database:', error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 // Export a function we will use to GET to the database.
